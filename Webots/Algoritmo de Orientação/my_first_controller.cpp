33fd9efc68994c413bbb4378280f8e12ecc74b32
Luan <luan.alflen4@gmail.com>
2023-10-24 21:06:13 -0300

    v2.0

--- Webots/Algoritmo de Orientação/my_first_controller.cpp
+++ Webots/Algoritmo de Orientação/my_first_controller.cpp
@@ -15,15 +15,15 @@ Motor *motor2 = robot->getMotor("motor_2");
 Gyro *gyro = robot->getGyro("gyro");
 
 double orientation = 0.0;
-double target = 0;
+double target = 0, previous_target=0.0;//previous não usando pra nada ainda nesse código
 
 void updateGyrol(int timeStep){
     orientation += (timeStep / 1000.0) * gyro->getValues()[2];
-    if(orientation > 3.1415){
-      orientation -= 2*3.1415; 
+    if(orientation > M_PI){
+      orientation -= 2*M_PI; 
     }
-    if(orientation < -3.1415){
-      orientation += 2*3.1415; 
+    if(orientation < -M_PI){
+      orientation += 2*M_PI; 
     }
   }
 
@@ -32,11 +32,11 @@ int main() {
   //const double F = 2.0;   // frequency 2 Hz
   double t = 0.0;         // elapsed simulation time
   double max_speed = 8.37758;
-  double base_speed = 1 * max_speed;
+  double base_speed = 0.0;
   double right_speed=0.0, left_speed=0.0;
-  double error = 0.0;
-  double Kp=15.0;
-  double proportional=0.0;
+  double error = 0.0, sum_error=0.0;
+  double Kp=15.0, Ki=0.1;
+  double proportional=0.0, integral=0.0;
  
   
   motor1->setPosition(INFINITY);
@@ -50,16 +50,30 @@ int main() {
   while (robot->step(TIME_STEP) != -1) {
     updateGyrol(TIME_STEP);
     
+    //escolhendo o formato do PID (um pra curvas abruscas e outro pra andar em linha reta)
+    if(abs(error) < 0.5){
+      Kp = 15.0;
+      base_speed = 1.0*max_speed;
+    }else{
+      Kp=4.0;
+      base_speed = 0.0*max_speed;
+    }
     
+    //corregindo erro pra não ter mudanças bruscas
     error = target - orientation;
     if (error > M_PI) {
         error -= 2 * M_PI;
     } else if (error < -M_PI) {
         error += 2 * M_PI;
     }
+    
+    // Cálculo do erro integrador
+    sum_error += error;
+    integral = Ki * sum_error;
+    
     proportional = Kp * error;
-    right_speed = base_speed + proportional;
-    left_speed = base_speed - proportional;
+    right_speed = base_speed + proportional + integral;
+    left_speed = base_speed - proportional - integral;
     
     //limitando a velocidade
     if(left_speed > max_speed){
@@ -78,20 +92,36 @@ int main() {
     
     motor2->setVelocity(left_speed);
     motor1->setVelocity(right_speed);
-    std::cout << "Target: " << target << " rad, response: " << orientation << " rad" << std::endl;
+    std::cout << "-----------------------------------------------------------------" << std::endl;
+    std::cout << "t: " << t << std::endl;
+    std::cout << "Target: " << target << ", P_target: " << previous_target << " rad, orientation: " << orientation << " rad" << std::endl;
+    std::cout << "dif: " << abs(previous_target - target) << ", error: " << error << ", Kp: " << Kp << std::endl;
+    
     t += (double)TIME_STEP / 1000.0;
     
     //caminho circular pré definido
-    if(t >2.5){
+    if(t > 2){
+      if(t==2){
+        previous_target = target;
+      }
       target = M_PI/2;
     }
-    if(t >5){
+    if(t > 4){
+      if(t==4){
+        previous_target = target;
+      }
       target = M_PI;
     }
-    if(t > 7.5){
+    if(t > 6){
+      if(t==6){
+        previous_target = target;
+      }
       target = -M_PI/2;
     }
-    if(t > 10){
+    if(t > 8){
+      if(t==8){
+        previous_target = target;
+      }
       target = 0;
       t=0;
     }
